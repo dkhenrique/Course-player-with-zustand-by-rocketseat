54af40d36e0f0ea622a70e2eb11ab2d88e83d912
dkhenrique <daniel.santos998@cs.unicid.edu.br>
2025-10-19 14:15:56 -0300

    feat: add vitest for testing and implement player slice tests

--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,7 +1,7 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit"
 import { UseAppSelector } from ".."
 
-const playerSlice = createSlice({
+export const playerSlice = createSlice({
   name: "player",
   initialState: {
     course: {
